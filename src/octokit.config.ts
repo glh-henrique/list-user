4a29e64c29c9d1d0554172cf76bc8f6ea44b4ef3
Guilherme H. Ricardo <glh..henrique@gmail.com>
2023-01-29 23:54:53 +0000

    ajustments variables enviroments

--- src/octokit.config.ts
+++ src/octokit.config.ts
@@ -1,5 +1,5 @@
 import { Octokit } from "octokit";
 
 export const octokit = new Octokit({
-  auth: "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" || process.env.REACT_APP_GITHUB_TOKEN
+  auth: process.env.REACT_APP_GITHUB_TOKEN
 })
